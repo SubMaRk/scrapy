97769d63610bfef158f0d6425c6b38cd48335989
Yutthaphon Inchaiya <yutthaphon30667@gmail.com>
2024-04-24 08:47:41 +0700

    Add moritoon config
    Add support moritoon.com

---
+++ include/madara/general/moritoon.py
@@ -0,0 +1,18 @@
+# CONFIG FILE FOR MORITOON.COM
+
+CONFIGURATIONS = {
+    "moritoon.com": {
+        "getsection": "div.site-content",
+        "gettitle": "div.post-title-custom h1",
+        "gettype": "Type",
+        "getgenre": "Genre(s)",
+        "getstatus": "Status",
+        "getchapterlist": "div.listing-chapters_wrap ul",
+        "getcover": "div.summary_image a img",
+        "getchaptertitle": "h1#chapter-heading",
+        "readdiv": "div.reading-content",
+        "delaylist": False,
+        "readjson": False,
+        "readencrypt": False
+    }
+}
