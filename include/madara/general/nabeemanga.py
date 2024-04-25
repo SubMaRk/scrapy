4fcd7fdaaf3a2f44a7667d8b74f51db6cecd2d46
Yutthaphon Inchaiya <yutthaphon30667@gmail.com>
2024-04-25 22:43:10 +0700

    Add nabee-manga template
    Add support nabee-manga.com

---
+++ include/madara/general/nabeemanga.py
@@ -0,0 +1,18 @@
+# CONFIG FILE FOR NABEE-MANGA.COM
+
+CONFIGURATIONS = {
+    "nabee-manga.com": {
+        "getsection": "div.site-content",
+        "gettitle": "div.post-title h1",
+        "gettype": "Type",
+        "getgenre": "Genre(s)",
+        "getstatus": "Status",
+        "getchapterlist": "div.listing-chapters_wrap ul",
+        "getcover": "div.summary_image a img",
+        "getchaptertitle": "h1#chapter-heading",
+        "readdiv": "div.reading-content",
+        "delaylist": False,
+        "readjson": False,
+        "readencrypt": True
+    }
+}
