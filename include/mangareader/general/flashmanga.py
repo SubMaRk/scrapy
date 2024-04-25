463fd959f6756217ad3fccfa221323df4bbfacb2
Yutthaphon Inchaiya <yutthaphon30667@gmail.com>
2024-04-25 22:44:45 +0700

    Add flash-manga template
    Add support flash-manga.com

---
+++ include/mangareader/general/flashmanga.py
@@ -0,0 +1,18 @@
+# CONFIG FILE FOR FLASH-MANGA.COM
+
+CONFIGURATIONS = {
+    "flash-manga.com": {
+        "getsection": "div.postbody",
+        "gettitle": "h1.entry-title",
+        "gettype": "imptdt",
+        "getgenre": "span.mgen",
+        "getstatus": "imptdt",
+        "getchapterlist": "div#chapterlist",
+        "getcover": "div.thumb img",
+        "getchaptertitle": "h1.entry-title",
+        "readdiv": "div#readerarea",
+        "delaylist": False,
+        "readjson": True,
+        "readencrypt": False
+    }
+}
