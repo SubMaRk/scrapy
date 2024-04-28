48b46f56cf5de2454278e6333094aa1513883a02
Yutthaphon Inchaiya <yutthaphon30667@gmail.com>
2024-04-28 13:11:21 +0700

    Create mangayipun.py
    Add support มังงะญี่ปุ่น.com

---
+++ include/mangareader/general/mangayipun.py
@@ -0,0 +1,18 @@
+# CONFIG FILE FOR มังงะญี่ปุ่น.COM
+
+CONFIGURATIONS = {
+    "xn--72cas2cj6a4hf4b5a8oc.com": {
+        "getsection": "div.postbody",
+        "gettitle": "h1.entry-title",
+        "gettype": "imptdt",
+        "getgenre": "span.mgen",
+        "getstatus": "imptdt",
+        "getchapterlist": "div#chapterlist",
+        "getcover": "div.thumb img",
+        "getchaptertitle": "h1.entry-title",
+        "readdiv": "div#readerarea",
+        "delaylist": False,
+        "readjson": True,
+        "readencrypt": False
+    }
+}
