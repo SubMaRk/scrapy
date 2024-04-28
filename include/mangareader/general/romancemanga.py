acd39f9a7a11315dffc910d83684bd842a49d78c
Yutthaphon Inchaiya <yutthaphon30667@gmail.com>
2024-04-28 13:09:52 +0700

    Create romancemanga.py
    Add support romance-manga.com

---
+++ include/mangareader/general/romancemanga.py
@@ -0,0 +1,18 @@
+# CONFIG FILE FOR ROMANCE-MANGA.COM
+
+CONFIGURATIONS = {
+    "romance-manga.com": {
+        "getsection": "div.series",
+        "gettitle": "div.series-title h2",
+        "gettype": "span.type",
+        "getgenre": "div.series-genres",
+        "getstatus": "span.status",
+        "getchapterlist": "ul.series-chapterlist",
+        "getcover": ".series-thumb img",
+        "getchaptertitle": "h2.title-chapter",
+        "readdiv": "div.reader-area",
+        "delaylist": False,
+        "readjson": False,
+        "readencrypt": False
+    }
+}
