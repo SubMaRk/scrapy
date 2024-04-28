6d89065e74c25d8f5dbc8b64389db97d49250c90
Yutthaphon Inchaiya <yutthaphon30667@gmail.com>
2024-04-28 13:11:09 +0700

    Create mangaza.py
    Add support manga-za.net

---
+++ include/mangareader/general/mangaza.py
@@ -0,0 +1,18 @@
+# CONFIG FILE FOR MANGA-ZA.NET
+
+CONFIGURATIONS = {
+    "manga-za.net": {
+        "getsection": "div.postbody",
+        "gettitle": "h1.entry-title",
+        "gettype": "Type",
+        "getgenre": "div.seriestugenre",
+        "getstatus": "Status",
+        "getchapterlist": "div#chapterlist",
+        "getcover": "div.thumb img",
+        "getchaptertitle": "h1.entry-title",
+        "readdiv": "div#readerarea",
+        "delaylist": False,
+        "readjson": True,
+        "readencrypt": False
+    }
+}
