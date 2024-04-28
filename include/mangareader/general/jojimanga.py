769908ec6f50b3ac69b3a7300f19757276b0fde7
Yutthaphon Inchaiya <yutthaphon30667@gmail.com>
2024-04-28 13:13:10 +0700

    Create jojimanga.py
    Add support joji-manga.com

---
+++ include/mangareader/general/jojimanga.py
@@ -0,0 +1,18 @@
+# CONFIG FILE FOR JOJI-MANGA.COM
+
+CONFIGURATIONS = {
+    "joji-manga.com": {
+        "getsection": "div.postbody",
+        "gettitle": "h1.entry-title",
+        "gettype": "imptdt",
+        "getgenre": "span.mgen",
+        "getstatus": "imptdt",
+        "getchapterlist": "div#chapterlist",
+        "getcover": "div.thumb img",
+        "getchaptertitle": "h1.entry-title",
+        "readdiv": "div#readerarea",
+        "delaylist": False,
+        "readjson": True,
+        "readencrypt": False
+    }
+}
