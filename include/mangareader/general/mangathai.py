6993e9e3458e4b0efb69d08d7d25f5e6a85caf44
Yutthaphon Inchaiya <yutthaphon30667@gmail.com>
2024-04-28 13:11:41 +0700

    Create mangathai.py
    Add support มังงะไทย.com

---
+++ include/mangareader/general/mangathai.py
@@ -0,0 +1,18 @@
+# CONFIG FILE FOR มังงะไทย.com
+
+CONFIGURATIONS = {
+    "xn--72ca2cvbi6fe9m.com": {
+        "getsection": "div.series",
+        "gettitle": "div.series-title h1",
+        "gettype": "span.type",
+        "getgenre": "div.series-genres",
+        "getstatus": "span.status",
+        "getchapterlist": "ul.series-chapterlist",
+        "getcover": ".series-thumb img",
+        "getchaptertitle": "h2.title-chapter",
+        "readdiv": "div.reader-area",
+        "delaylist": False,
+        "readjson": False,
+        "readencrypt": True
+    }
+}
