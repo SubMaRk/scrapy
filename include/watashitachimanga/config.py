e615e076a9fb55120921763aba039f8ed8b0d7f7
Yutthaphon Inchaiya <yutthaphon30667@gmail.com>
2024-05-19 23:19:29 +0700

    Pre support watashitachimanga

---
+++ include/watashitachimanga/config.py
@@ -0,0 +1,18 @@
+# CONFIG FILE FOR WATASHITACHIMANGA.COM
+
+CONFIGURATIONS = {
+    "watashitachimanga.com": {
+        "getsection": "section.content",
+        "gettitle": "h1.page-header",
+        "gettype": "",
+        "getgenre": "",
+        "getstatus": "",
+        "getchapterlist": "ul.list-group",
+        "getcover": "img.thumbnail",
+        "getchaptertitle": "",
+        "readdiv": "div.container",
+        "delaylist": False,
+        "readjson": False,
+        "readencrypt": False
+    }
+}
